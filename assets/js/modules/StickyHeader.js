7b530db6b966ed36ff11786f9871cd53a2b60b4a
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2017-02-22 22:22:42 +0200

    Full price finished, sticky menu fixes

--- assets/js/modules/StickyHeader.js
+++ assets/js/modules/StickyHeader.js
@@ -4,25 +4,22 @@ import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoi
 class StickyHeader {
     constructor() {
         this.siteHeader = $(".site-header");
-        this.headerTrigger = $(".hero");
+        this.headerTrigger = $(".site-main");
         this.createHeaderWaypoint();
-        console.log('constructor');
     }
 
     createHeaderWaypoint() {
-        console.log('waypoints');
         var self = this;
         new Waypoint({
             element: this.headerTrigger[0],
             handler: function(direction) {
-
-                console.log('scroll');
                 if (direction == "down") {
                     self.siteHeader.addClass("site-header--compact");
                 } else {
                     self.siteHeader.removeClass("site-header--compact");
                 }
-            }
+            },
+            offset: 50
         });
     }
 }
